b6589f12512589fd3e48828a1fc7160cbf239535
chinh.dovan <dochinhthtn@gmail.com>
2023-02-19 20:29:24 +0700

    Hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import { Routes, Route, Link, BrowserRouter, HashRouter } from "react-router-dom";
 
 function App() {
 
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Link to="/">Trang chủ</Link> <br />
       <Link to="/contact">Liên hệ</Link><br />
       <Link to="/about">Giới thiệu</Link><br />
@@ -17,7 +17,7 @@ function App() {
         <Route path="/contact" element={<h1>Đây là liên hệ</h1>} />
         <Route path="/about" element={<h1>Đây là trang giới thiệu</h1>} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
